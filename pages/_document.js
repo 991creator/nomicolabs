a13b677a4edafd76c0187c4e60299eaadc4bb4bf
991creator <126946426+991creator@users.noreply.github.com>
2023-07-12 23:20:58 +0400

    Fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Orbit&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet" />
         </Head>
         <body>
             <Main/>
