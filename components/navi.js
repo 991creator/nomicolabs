f20acd6e56d0bf7f1aff6d1cd84c24f8454bfa6e
991creator <126946426+991creator@users.noreply.github.com>
2023-07-19 16:18:08 +0400

    fix update

--- components/navi.js
+++ components/navi.js
@@ -11,7 +11,7 @@ export default function Navi () {
         <>
          <div className="flex flex-row justify-between items-center p-[10px]">
       <Link href="/"><p className="ml-[25px] text-2xl md:text-3xl p-[10px] text-white font-bold">{'Nomico Labs'}</p></Link>
-      <div className="flex items-center justify-end z-10">
+      <div className="flex items-center justify-start z-10">
         <button className="p-[25px] bg-slate-950 rounded-full" onClick={() => handleNav()}><Image src="/whiteNav.jpeg" width="50" height="50" alt="logo"></Image></button>
       </div>
       <div className={state ? "top-0 left-0 right-0 absolute h-screen w-screen flex flex-col justify-center items-center bg-white ease-in duration-300 text-black" : "top-0 left-[-100%] right-0 ease-in duration-300 absolute h-screen w-screen flex flex-col justify-center items-center bg-white text-black"}>
