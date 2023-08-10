f192b74e913797227ef91b024433c421c6c90e9f
991creator <126946426+991creator@users.noreply.github.com>
2023-08-10 14:48:16 +0300

    Fixes

--- pages/projects.js
+++ pages/projects.js
@@ -22,8 +22,8 @@ const Proje = () => {
     <Link href="https://sageport.vercel.app/" target="_blank" className="m-[20px]">
     <div className="transition duration-150 ease-out p-[15px] hover:ease-in flex flex-col hover:bg-gray-200 justify-center items-center h-[400px] w-[300px] bg-white border-2 border-black">
       <p className="text-4xl">SagePort</p>
-      <p className="text-xl my-[15px]">#WEB3 safe storage</p>
-      <p className="text-base mt-[25px] text-xl">Private cells with decentralized ownership stored on Blockchain!</p>
+      <p className="text-xl my-[15px]">#WEB3 donation</p>
+      <p className="text-base mt-[25px] text-xl">Decentralized and private donation platform for convenient fundraising!</p>
     </div>
     </Link>
     <Link href="https://floatblock.vercel.app/" target="_blank" className="m-[20px]">
